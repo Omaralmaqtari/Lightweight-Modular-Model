fa7588eec956290831c3c34cefa0f6951859958a
Omaralmaqtari <89450984+Omaralmaqtari@users.noreply.github.com>
2023-12-19 18:40:21 +0800

    Update flop.py

--- flop.py
+++ flop.py
@@ -1,11 +1,11 @@
-import torchvision.models as models
+from LM_Net import LM_Net
 import torch
 from ptflops import get_model_complexity_info
 import re
 
 #Model thats already available
-net = models.densenet161()
-macs, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=False, verbose=True)
+model = LM_Net(3,1)
+macs, params = get_model_complexity_info(model, (3, 224, 224), as_strings=True, print_per_layer_stat=False, verbose=True)
 # Extract the numerical value
 flops = eval(re.findall(r'([\d.]+)', macs)[0])*2
 # Extract the unit
